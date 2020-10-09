0e0afc0ba582351f82ef80c87284ae06154b6f8b
Souptik <souptikdatta2001@gmail.com>
2020-10-09 23:10:31 +0530

    Fixed url duplication error (#1)

--- js/background.js
+++ js/background.js
@@ -7,39 +7,30 @@ function copyToClipboard(text) {
     document.body.removeChild(input);
 }
 
-getCurrentTabUrl = function() {
-    return new Promise(function(resolve, reject) {
-        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-            let currentUrl = tabs[0].url;
-            resolve(currentUrl);
-        });
-    });
-}
-
 prepareChromepoint = function(payload) {
     return new Promise(function(resolve, reject) {
-        getCurrentTabUrl().then(function(currentUrl) {
-            let pinpointedText = payload.selectionText;
-            let pinpointedTextEncoded = encodeURI(pinpointedText);
-        
-            let pinpointedUrl = currentUrl + "#:~:text=" + pinpointedTextEncoded;
-            console.log(pinpointedUrl)
-            resolve(pinpointedUrl);
-        });
+        let pinpointedText = payload.selectionText;
+        let pinpointedTextEncoded = encodeURI(pinpointedText);
+        let currentUrl = payload.pageUrl;
+        const regex = /#:~:.+/g;
+        currentUrl = currentUrl.replace(regex, '');
+        let pinpointedUrl = currentUrl + "#:~:text=" + pinpointedTextEncoded;
+        console.log(pinpointedUrl);
+        resolve(pinpointedUrl);
     });
-}
+};
 
 chromepointToNewTab = function(payload) {
     prepareChromepoint(payload).then(function(chromepointedUrl) {
         chrome.tabs.create({url: chromepointedUrl});
     });
-}
+};
 
 chromepointToClipboard = function(payload) {
     prepareChromepoint(payload).then(function(chromepointedUrl) {
         copyToClipboard(chromepointedUrl);
     });
-}
+};
 
 chrome.contextMenus.create({
     title: "chromepoint it in a new tab",
